e21b7519f738db08c8ad3936a220e022cd9d4ab5
a.dolgov <dolgov_am@mail.ru>
2025-04-18 17:02:27 +0300

    update root

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from api.rotues import router as gameMer_roter
 from fastapi.templating import Jinja2Templates
 
@@ -8,6 +8,11 @@ templates = Jinja2Templates(directory='templates')
 app = FastAPI(description='Maks Plaksin')
 app.include_router(gameMer_roter)
 
+
+@app.get('/')
+def root(request: Request):
+    return templates.TemplateResponse('Root.html', {'request': request})
+
+
 if __name__ == '__main__':
     uvicorn.run('main:app', port=8080, reload=True)
-
